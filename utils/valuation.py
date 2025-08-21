d9f18922e08211b005f674582214cbd25ee7be73
tathagatasadhu-beep <tathagata.sadhu@gmail.com>
2025-08-21 09:53:46 -0400

    Create valuation.py

---
+++ utils/valuation.py
@@ -0,0 +1,10 @@
+def dcf_valuation(data, discount_rate=10, growth_rate=5, years=5):
+    prices = data.get("c", [])
+    if not prices:
+        return 0
+    last_price = prices[-1]
+    cash_flow = last_price * 0.1
+    value = 0
+    for i in range(1, years+1):
+        value += cash_flow*((1+growth_rate/100)**i)/((1+discount_rate/100)**i)
+    return value
