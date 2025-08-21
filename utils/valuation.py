35276847622ab2fdea86838c2a7b62923d41024f
tathagatasadhu-beep <tathagata.sadhu@gmail.com>
2025-08-21 11:41:20 -0400

    Update valuation.py

--- utils/valuation.py
+++ utils/valuation.py
@@ -3,7 +3,7 @@ def dcf_valuation(data, discount_rate=10, growth_rate=5, years=5):
     if not prices:
         return 0
     last_price = prices[-1]
-    cash_flow = last_price * 0.1
+    cash_flow = last_price*0.1
     value = 0
     for i in range(1, years+1):
         value += cash_flow*((1+growth_rate/100)**i)/((1+discount_rate/100)**i)
